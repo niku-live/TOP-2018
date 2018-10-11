000a4d2c22f175d7325952953c739f7ca45a2853
Karolis Uosis <nku2007@hotmail.com>
2018-10-11 08:41:19 +0300

    Practice 4

---
+++ Practice-04/Practice-04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new Comparable.Example2().RunExample();
+            //new Comparer.Example1().RunExample();
+            //new Equitable.Example1().RunExample();
+            new Yield.Example1().RunExample();   
+        }
+    }
+}
---
+++ Practice-04/Practice-04/Yield/Example1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_04.Yield
+{
+    public class Example1
+    {
+        IEnumerable<string> TestMethod(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return i.ToString();
+            }
+            yield return "Final";
+        }
+
+        public void RunExample()
+        {
+            var enumerator = TestMethod(5).GetEnumerator();
+
+            var list = TestMethod(5).ToList();
+
+            foreach (var item in TestMethod(5))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
