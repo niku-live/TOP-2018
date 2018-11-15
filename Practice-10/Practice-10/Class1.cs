435df6238e8353fe021be8a495e1d20257c60def
Karolis Uosis <nku2007@hotmail.com>
2018-11-15 10:49:34 +0200

    Lecture 10

---
+++ Practice-10/Practice-10/Class1.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_10
+{
+    public class Rootobject
+    {
+        public Restresponse RestResponse { get; set; }
+    }
+
+    public class Restresponse
+    {
+        public string[] messages { get; set; }
+        public Result[] result { get; set; }
+    }
+
+    public class Result
+    {
+        public string name { get; set; }
+        [JsonProperty("alpha2_code")]
+        public string Alpha2Code { get; set; }
+        public string alpha3_code { get; set; }
+    }
+
+    class Class1
+    {
+    }
+}
